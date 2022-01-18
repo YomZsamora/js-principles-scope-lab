4712aec186ac3c96402c9f0a107690a477449470
yomZsamora <samora.y@adzumi.co.ke>
2022-01-18 08:11:42 +0300

    Function called overwriteBestCustomer() that changes that bestCustomer global variable declared in setBestCustomer().

--- index.js
+++ index.js
@@ -4,10 +4,14 @@ var customerName = 'bob';
 // Function that accesses that global customerName variable, and uppercases it.
 function upperCaseCustomerName() {
     customerName = customerName.toUpperCase();
-
 }
 
 // Function that when called, declares a variable called bestCustomer in global scope and assigns it to be 'not bob'.
 function setBestCustomer() {
     bestCustomer = 'not bob';
+}
+
+// Function called overwriteBestCustomer() that changes that bestCustomer global variable declared in setBestCustomer()
+function overwriteBestCustomer(newBestCustomer) {
+    bestCustomer = newBestCustomer;
 }
